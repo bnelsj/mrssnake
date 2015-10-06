ed206c6ebfa5c5991be71e54010fd82d1547b2aa
Brad Nelson <bnelsj@gmail.com>
2015-10-06 14:40:46 -0700

    If chr is 'unmapped', assume sam input from samtools view

--- chunker.py
+++ chunker.py
@@ -6,23 +6,31 @@ import argparse
 import pysam
 import numpy as np
 
-SEND_BLOCK_SIZE = int(2e6)
-READ_BLOCK_SIZE = int(10e6) # Chunk size for input
+def get_fetch_list(chr, start, end):
+    if start is None or end is None:
+        fetch_list = chr
+    else:
+        fetch_list = [chr, start, end]
+    return fetch_list
 
-def chunk_reads(chr, start, end, bamfile, outfile, chunk_size, fifo):
+def chunk_read(read, outfile, chunk_size):
+    n_to_do = read.rlen // chunk_size
+    for k in range(n_to_do):
+        outfile.write(">0\n" + read.seq[k*chunk_size : k*chunk_size + chunk_size] + "\n")
+
+def chunk_reads(args, fetch_string):
     """This takes blocks of reads from a bam, then chunks them and sends them to mrsfast"""
 
-    if chr == "unmapped":
-        fetch_string = "*"
-    elif start is None or end is None:
-        fetch_string = chr
+    outfile = open(args.outfile, "w")
+    if args.chr == "unmapped": # Assumes unmapped reads passed as sam by samtools view "*"
+        with open(args.bamfile, "r") as input:
+            for read in input:
+                chunk_read(read, outfile, args.chunk_size)
     else:
-        fetch_string = "%s:%d-%d" % (chr, start + 1, end - 1)
-
-    for l in pysam.view(bamfile, fetch_string):
-        n_to_do = l.rlen // chunk_size
-        for k in range(n_to_do):
-            outfile.write(">0\n" + l.seq[k*chunk_size : k*chunk_size + chunk_size] + "\n")
+        bamfile = pysam.AlignmentFile(args.bamfile)
+        fetch_list = get_fetch_list(args.chr, args.start, args.end)
+        for read in bamfile.fetch(*fetch_list, until_eof=True):
+            chunk_read(read, outfile, args.chunk_size)
 
     #Handle regions where there are no reads
     try:
@@ -33,36 +41,9 @@ def chunk_reads(chr, start, end, bamfile, outfile, chunk_size, fifo):
                 fifo_handle.write("ERROR: no reads for %s\n" % fetch_string)
         outfile.write("\n")
         sys.stderr.write("No reads for %s\n" % fetch_string)
-    
-def chunk_reads_old(chr, start, end, bamfile, outfile, chunk_size, dt):
-    """This takes blocks of reads from a bam, then chunks them and sends them to mrsfast
-       Code from psudmant's super_mapper.py"""
-    reads_block = np.empty(SEND_BLOCK_SIZE, dtype = dt)
-
-    curr_pos = 0
-    for l in bamfile.fetch(chr, start, end):
-        n_to_do = l.rlen // chunk_size
-        for k in range(n_to_do):
-            reads_block[curr_pos] = l.seq[k*chunk_size: k*chunk_size + chunk_size]
-            curr_pos += 1
-            if curr_pos == SEND_BLOCK_SIZE: 
-                break
-        if curr_pos == SEND_BLOCK_SIZE:
-            curr_pos = 0
-            outfile.write(reads_block)
-            #READER_client_SEND_READS(reads_block,SEND_BLOCK_SIZE,proc_name,myrank,comm,icomm_to_RUNNER)
-    
-    if curr_pos!=0:
-        small_reads_block = np.empty(curr_pos,dtype = dt)
-        small_reads_block[:] = reads_block[:curr_pos]
-        outfile.write(small_reads_block)
-        #READER_client_SEND_READS(small_reads_block,curr_pos,proc_name,myrank,comm,icomm_to_RUNNER)
-
-    ### make a new little block and copy stuff into it then send out
-    #assert True
-
-    print("FINISHED READING %s:%d-%d" % (chr,start,end))
-
+    finally:
+        bamfile.close()
+        outfile.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -76,10 +57,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    #bamfile = pysam.AlignmentFile(args.bamfile, "rb")
-    outfile = open(args.outfile, "w")
-
-    chunk_reads(args.chr, args.start, args.end, args.bamfile, outfile, args.chunk_size, args.fifo)
-
-    #bamfile.close()
-    outfile.close()
+    chunk_reads(args, fetch_string)
