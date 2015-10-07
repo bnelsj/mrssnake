bf7715a3e43480799b2a23198baf8defe7b60573
Brad Nelson <bnelsj@gmail.com>
2015-10-07 16:40:37 -0700

    Require fifo argument. Added logging output.

--- chunker.py
+++ chunker.py
@@ -8,53 +8,58 @@ import numpy as np
 
 def get_fetch_list(chr, start, end):
     if start is None or end is None:
-        fetch_list = chr
+        fetch_list = [chr]
     else:
         fetch_list = [chr, start, end]
     return fetch_list
 
-def chunk_read(read, outfile, chunk_size):
+def chunk_read(i, read, outfile, chunk_size, write_step = 1000000):
+    "Break a read into kmers"
     n_to_do = read.rlen // chunk_size
     for k in range(n_to_do):
         outfile.write(">0\n" + read.seq[k*chunk_size : k*chunk_size + chunk_size] + "\n")
 
-def chunk_reads(args, fetch_string):
-    """This takes blocks of reads from a bam, then chunks them and sends them to mrsfast"""
-
-    outfile = open(args.outfile, "w")
-    if args.chr == "unmapped": # Assumes unmapped reads passed as sam by samtools view "*"
-        with open(args.bamfile, "r") as input:
-            for read in input:
-                chunk_read(read, outfile, args.chunk_size)
-    else:
-        bamfile = pysam.AlignmentFile(args.bamfile)
-        fetch_list = get_fetch_list(args.chr, args.start, args.end)
-        for read in bamfile.fetch(*fetch_list, until_eof=True):
-            chunk_read(read, outfile, args.chunk_size)
-
-    #Handle regions where there are no reads
-    try:
-        l
-    except NameError:
-        if args.fifo is not None:
-            with open(fifo, "w") as fifo_handle:
-                fifo_handle.write("ERROR: no reads for %s\n" % fetch_string)
-        outfile.write("\n")
-        sys.stderr.write("No reads for %s\n" % fetch_string)
-    finally:
-        bamfile.close()
-        outfile.close()
+    if i % write_step == 0:
+        sys.stderr.write("Chunker: %d reads chunked\n" % i)
+        sys.stderr.flush()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("bamfile")
     parser.add_argument("chr", help="Input bam contig to chunk. Use 'unmapped' to get all unmapped reads.")
+    parser.add_argument("fifo", help = "Path to fifo file")
     parser.add_argument("--start", type=int)
     parser.add_argument("--end", type=int)
     parser.add_argument("--outfile", default = "/dev/stdout")
     parser.add_argument("--chunk_size", default = 36, type = int)
-    parser.add_argument("--fifo", help = "Path to fifo file")
 
     args = parser.parse_args()
 
-    chunk_reads(args, fetch_string)
+    outfile = open(args.outfile, "w")
+    bamfile = pysam.AlignmentFile(args.bamfile)
+
+    fetch_list = get_fetch_list(args.chr, args.start, args.end)
+
+    msg = "Chunker: Chunking %s reads\n" % " ".join(map(str, fetch_list))
+    sys.stderr.write(msg)
+
+    if args.chr == "unmapped": # Assumes unmapped reads passed as sam by samtools view "*"
+        for i, read in enumerate(bamfile.fetch(until_eof=True)):
+            chunk_read(i, read, outfile, args.chunk_size)
+    else:
+        for i, read in enumerate(bamfile.fetch(*fetch_list, until_eof=True)):
+            chunk_read(i, read, outfile, args.chunk_size) 
+    bamfile.close()
+
+    #Handle regions where there are no reads
+    try:
+        sys.stderr.write("Chunker: finished chunking %d reads to chunk\n" % i)
+    except NameError:
+        with open(args.fifo, "w") as fifo:
+            fifo.write("Chunker: found no reads to chunk\n")
+        outfile.write("\n")
+        sys.stderr.write("Chunker: found no reads to chunk\n")
+    finally:
+        outfile.close()
+
+    sys.exit()
