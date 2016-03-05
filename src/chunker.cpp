15245d0ddb9676a43088baa753b7e5f727b100f4
Zev N. Kronenberg <zevk@e152.grid.gs.washington.edu>
2016-03-04 16:30:01 -0800

    still broken but closer

--- src/chunker.cpp
+++ src/chunker.cpp
@@ -45,6 +45,7 @@ THE SOFTWARE.
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <inttypes.h>
 #include "htslib/hts.h"
 #include "htslib/bgzf.h"
 #include "htslib/sam.h"
@@ -53,11 +54,11 @@ THE SOFTWARE.
 
 struct interval
 {
-    int64_t start,end;
-	bool operator < (const interval& other) const
-	{
-		return (start < other.start);
-	}
+  int64_t start, end;
+  bool operator < (const interval& other) const
+  {
+    return (start < other.start);
+  }
 };
 
 struct options{
@@ -71,46 +72,45 @@ static const char *optString = "b:p:n:c";
 
 //-------------------------------   OPTIONS   --------------------------------
 int parseOpts(int argc, char** argv)
-    {
-    int opt = 0;
-    globalOpts.file = "NA";
-	globalOpts.chunk_size = 36;
-    opt = getopt(argc, argv, optString);
-    while(opt != -1){
-	switch(opt){
-		case 'h':
-		{
-		 std::cerr << "Useage" << std::endl;
-		}
-		case 'b':
-		{
-		 globalOpts.file = optarg;
-		 break;
-		}
-		case 'p':
-		{
-		 globalOpts.part = atoi(optarg);
-		 break;
-		}
-		case 'n':
-		{
-		 globalOpts.nparts = atoi(optarg);
-		 break;
-		}
-		case 'c':
-		{
-		 globalOpts.chunk_size = atoi(optarg);
-		 break;
-		}
-		case '?':
-		{
-		 break;
-		}
-	}
- 
-  opt = getopt( argc, argv, optString ); 
-   }
-return 1;
+{
+  int opt = 0;
+  globalOpts.file = "NA";
+  globalOpts.chunk_size = 36;
+  opt = getopt(argc, argv, optString);
+  while(opt != -1){
+    switch(opt){
+    case 'h':
+      {
+	std::cerr << "Useage" << std::endl;
+      }
+    case 'b':
+      {
+	globalOpts.file = optarg;
+	break;
+      }
+    case 'p':
+      {
+	globalOpts.part = atoi(optarg);
+	break;
+      }
+    case 'n':
+      {
+	globalOpts.nparts = atoi(optarg);
+	break;
+      }
+    case 'c':
+      {
+	globalOpts.chunk_size = atoi(optarg);
+	break;
+      }
+    case '?':
+      {
+	break;
+      }
+    }
+    opt = getopt( argc, argv, optString ); 
+  }
+  return 1;
 }
 
 inline void initKstring(kstring_t * k){
@@ -154,48 +154,44 @@ std::vector<struct interval> get_chunk_range(std::vector<struct interval> chunks
  Function returns: 0 if successful, 1 otherwise
 
 */
-int get_reads(std::vector<struct interval> &chunks, std::string bamfile)
-{
-	BGZF* bam;
-	
-	kstring_t tmp, seq;
-	initKstring(&tmp);
-	initKstring(&seq);
-	int seek;
-	if(bgzf_is_bgzf == 0) {
-		std::cerr << "Error: file is not in BGZF format." << std::endl;
-		exit(1);
-	}
-	bam = bgzf_open(bamfile.c_str(), "r");
-	// Get BGZF file handle
+int get_reads(std::vector<struct interval> & chunks, 
+	      std::string bamfile){
+  BGZF* bam;
+  kstring_t tmp, seq;
+  initKstring(&tmp);
+  initKstring(&seq);
+  int seek;
+  if(bgzf_is_bgzf == 0) {
+    std::cerr << "Error: file is not in BGZF format." << std::endl;
+    exit(1);
+  }
+  bam = bgzf_open(bamfile.c_str(), "r");
+    
 
-    for(int i = 0; i < chunks.size(); i++){
-		int state = 1;
-		seek = bgzf_seek(bam, chunks[i].start, 0);
-		if(seek != 0) {
-			std::cerr << "Error: could not seek to position " << chunks[i].start << "." << std::endl;
-			exit(1);
-		}
-		std::cout << "Going to position " << chunks[i].start << " " << chunks[i].end << std::endl;
+  uint64_t of = 543039114950783;
 
-		while(state > 0 and state < chunks[i].end) {
-		//	for(int j =0; j < 9; j++) {
-		//		state = bgzf_getline(bam, '\t', &tmp);
-		//	}
-			state = bgzf_getline(bam, '\n', &seq);
-		//	state = bgzf_getline(bam, '\n', &tmp);
+  if(bgzf_seek(bam, of, 0) == -1){
+    std::cerr << "Error could not seek. " << std::endl;
+    exit(1);
+  }
+  if(bgzf_getline(bam, '\n', &seq)){
+    
+    std::cerr << seq.s << std::endl;
+    
+  }
+  if(bgzf_getline(bam, '\n', &seq)){
+    std::cerr << seq.s << std::endl;
+  }
+  if(bgzf_getline(bam, '\n', &seq)){
+    std::cerr << seq.s << std::endl;
+  }
+  if(bgzf_getline(bam, '\n', &seq)){
+    std::cerr << seq.s << std::endl;
+  }
 
-			std::cout << seq.s << std::endl;
-		}
-		
-		//Seek to offset
-		//Read until end
-			//Split reads
-    }
-	bgzf_close(bam);
-
-    return 0;
+  bgzf_close(bam);
 }
+  
 //------------------------------- SUBROUTINE --------------------------------
 /*
  Function input  : options
@@ -206,62 +202,63 @@ int get_reads(std::vector<struct interval> &chunks, std::string bamfile)
 
 */
 
-std::vector<struct interval> read_index(std::string fn, int part, int nparts) {
-	const char * file_name = fn.c_str();
-	std::string index_name = fn + ".bai";
-	long chunk_counter=0;
-	FILE *ptr_myfile;
-	std::vector <struct interval> chunks, chunks_to_read;
-
-	ptr_myfile = fopen(index_name.c_str(),"rb");
-
-	if(!ptr_myfile) {
-		std::cerr << "Unable to open file." << std::endl;
-		exit(1);
-	}
-
-	char magic[4];
-	fread(&magic, sizeof(char), 4, ptr_myfile);	
-	printf("magic:%s\n",magic);
-
-    int32_t n_ref;
-    fread(&n_ref,sizeof(int32_t),1,ptr_myfile);
-    printf("n_ref:%d\n",n_ref);
-
+void read_index(std::vector<struct interval> & chunks, 
+		std::string fn, int part, int nparts) {
+  const char * file_name = fn.c_str();
+  std::string index_name = fn + ".bai";
+  long chunk_counter=0;
+  FILE *ptr_myfile;
+  //std::vector <struct interval> chunks;
+  
+  ptr_myfile = fopen(index_name.c_str(),"rb");
+  
+  if(!ptr_myfile) {
+    std::cerr << "Unable to open file." << std::endl;
+    exit(1);
+  }
+  
+  char magic[4];
+  fread(&magic, sizeof(char), 4, ptr_myfile);	
+  printf("magic:%s\n",magic);
+  
+  int32_t n_ref;
+  fread(&n_ref,sizeof(int32_t),1,ptr_myfile);
+  printf("n_ref:%d\n",n_ref);
+  
+  int32_t nn;
+  for(nn = 0; nn < n_ref; ++nn){
+    
     int32_t n_bin;
     fread(&n_bin,sizeof(int32_t),1,ptr_myfile);
     printf("n_bin:%d\n",n_bin);
-
-	int32_t i;
+    
+    int32_t i;
     for (i = 0; i < n_bin; ++i) {
-        uint32_t bin; 
-        fread(&bin,sizeof(uint32_t),1,ptr_myfile);
-
-        int32_t n_chunk;
-        fread(&n_chunk,sizeof(int32_t),1,ptr_myfile);
-		chunk_counter += n_chunk;
-        int32_t j;
-        for (j = 0; j < n_chunk; ++j) {
-            int64_t chunk_beg, chunk_end;
-			struct interval tmp;
-            fread(&chunk_beg,sizeof(int64_t),1,ptr_myfile);
-            fread(&chunk_end,sizeof(int64_t),1,ptr_myfile);
-
-			tmp.start = chunk_beg;
-			tmp.end = chunk_end;
-			chunks.push_back(tmp);		
-        }
-
+      uint32_t bin; 
+      fread(&bin,sizeof(uint32_t),1,ptr_myfile);
+      
+      int32_t n_chunk;
+      fread(&n_chunk,sizeof(int32_t),1,ptr_myfile);
+      chunk_counter += n_chunk;
+      int32_t j;
+      for (j = 0; j < n_chunk; ++j) {
+	int64_t chunk_beg, chunk_end;
+	interval tmp;
+	fread(&chunk_beg,sizeof(int64_t),1,ptr_myfile);
+	fread(&chunk_end,sizeof(int64_t),1,ptr_myfile);
+		
+	tmp.start = chunk_beg;
+	tmp.end = chunk_end;
+	chunks.push_back(tmp);		
+      }
     }
-	printf("Total chunks: %d\n", chunk_counter);
-	chunks_to_read = get_chunk_range(chunks, part, nparts);	
-	fclose(ptr_myfile);
-	printf("Vector size: %d\n", chunks.size());
-	printf("Chunks to read: %d\n", chunks_to_read.size());
-
-
-	return chunks_to_read;
-	
+  }
+  printf("Total chunks: %d\n", chunk_counter);
+  // chunks_to_read = get_chunk_range(chunks, part, nparts);	
+  fclose(ptr_myfile);
+  printf("Vector size: %d\n", chunks.size());
+  printf("Chunks to read: %d\n", chunks.size());
+  
 }
 
 
@@ -296,53 +293,42 @@ std::string chunk_read(std::string &read, int chunk_size)
 
 int main( int argc, char** argv)
 {
+  globalOpts.chunk_size = 36;
+  globalOpts.part = -1;
+  globalOpts.nparts = -1;
+  int parse = parseOpts(argc, argv);
+  
+  if (globalOpts.part == -1) {
+    std::cerr << "Error: no partition specified" << std::endl;
+    exit(1);
+  } 
+  if (globalOpts.nparts == -1) {
+    std::cerr << "Error: number of partitions not specified" << std::endl;
+    exit(1);
+  }  
+  if (!(globalOpts.part < globalOpts.nparts)) {
+    std::cerr << "Error: partition number must be less than total number of partitions (Partition numbers are 0-based)" << std::endl;
+    exit(1);
+  }
+  
+  if (globalOpts.file.empty()) {
+    std::cerr << "Error: no bam file provided" << std::endl;
+    exit(1);
+  }
+  
+  samFile *in = sam_open(globalOpts.file.c_str(), "r");
+  if(in == NULL) {
+    std::cerr << "Unable to open BAM/SAM file." << std::endl;
+    exit(1);
+  }
 
-    globalOpts.chunk_size = 36;
-	globalOpts.part = -1;
-	globalOpts.nparts = -1;
-	std::vector<struct interval> chunks_to_read;
-    int parse = parseOpts(argc, argv);
+  hts_idx_t *idx = sam_index_load(in, globalOpts.file.c_str());
+  
+  std::vector<interval> chunks_to_read;
 
-    if (globalOpts.part == -1) {
-        std::cerr << "Error: no partition specified" << std::endl;
-        exit(1);
-    }
-
-     if (globalOpts.nparts == -1) {
-        std::cerr << "Error: number of partitions not specified" << std::endl;
-        exit(1);
-    }
-
-	if (!(globalOpts.part < globalOpts.nparts)) {
-		std::cerr << "Error: partition number must be less than total number of partitions (Partition numbers are 0-based)" << std::endl;
-		exit(1);
-	}
-
-    if (globalOpts.file.empty()) {
-        std::cerr << "Error: no bam file provided" << std::endl;
-		exit(1);
-    }
-
-	samFile *in = sam_open(globalOpts.file.c_str(), "r");
-	if(in == NULL) {
-    	std::cerr << "Unable to open BAM/SAM file." << std::endl;
-		exit(1);
-    }
-	hts_idx_t *idx = sam_index_load(in, globalOpts.file.c_str());
-
-	chunks_to_read = read_index(globalOpts.file, globalOpts.part, globalOpts.nparts);
-	int result = get_reads(chunks_to_read, globalOpts.file.c_str());	
-
-    //    idx = get_positions(globalOpts);
-//    std::cout << idx << std::endl;   
-// Get index from bamfile
-// Open index
-// Get total chunks
-// Calculate start_pos and end_pos
-// Get offset of start_pos and end_pos
-// Seek to start_pos
-   // Chunk reads
-// Read until end_pos
-
-    return 0;
+  read_index(chunks_to_read, globalOpts.file, 
+	     globalOpts.part, globalOpts.nparts);
+  int result = get_reads(chunks_to_read, globalOpts.file.c_str());
+  
+  return 0;
 }
